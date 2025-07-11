a225d8fc11cff54ded219d33d1a1e3a3a6207003
PKAngel123 <158393329+PKAngel123@users.noreply.github.com>
2025-07-11 16:34:26 -0300

    Add files via upload

--- conexion.js
+++ conexion.js
@@ -1,17 +1,16 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const conexion = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    database: process.env.MYSQLDATABASE,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    port: process.env.MYSQLPORT
+    host: "localhost",
+    database: "servicios",
+    user: "root",
+    password: ""
 });
 
 conexion.connect(function(err){
     if(err){
         throw err;
     }else{
-        console.log("conexion exitosa");
+    console.log("conexion exitosa");
     }
 });
 

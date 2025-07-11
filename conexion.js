ab01f5888820a33b39f28bc17c929a47ca23eaa4
PKAngel123 <158393329+PKAngel123@users.noreply.github.com>
2025-07-11 17:02:13 -0300

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql2');
 const conexion = mysql.createConnection({
-    host: "localhost",
-    database: "servicios",
-    user: "root",
-    password: ""
+    host: process.env.MYSQLHOST || "localhost",
+    database: process.env.MYSQLDATABASE || "servicios",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "",
+    port: process.env.MYSQLPORT || 3306
 });
 
 conexion.connect(function(err){
     if(err){
         throw err;
     }else{
-    console.log("conexion exitosa");
+        console.log("conexion exitosa");
     }
 });
 
